0ec7b5bf30d24d4e38349a9c7c4a104f457eceb6
James Madden <jamesbmadden@gmail.com>
2022-01-06 13:07:40 -0800

    Add map push functionality

--- src/workers/buildings.ts
+++ src/workers/buildings.ts
@@ -2,10 +2,20 @@ import * as Comlink from "comlink";
 
 class Buildings {
 
-  map = new Int8Array([0]);
+  map = new Int8Array([
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
+
+  _cb: Function;
 
   static log () {
     console.log('buildings worker running');
+  }
+
+  setCallback (cb) {
+    this._cb = cb;
+    this._cb(this.map);
   }
 
 }

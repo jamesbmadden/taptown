1c1d0d7609fc1c40c659b4e20d7b5208aa1bc60e
James Madden <jamesbmadden@gmail.com>
2022-01-05 20:24:55 -0800

    remove old test implementation

--- src/model.ts
+++ src/model.ts
@@ -133,54 +133,6 @@ export default function loadModel (gl: WebGLRenderingContext, url: string): Prom
 
     });
 
-    // operate on every node in scene
-    /*scene.nodes.forEach(nodeId => {
-
-      // get node
-      const node = modelJson.nodes[nodeId];
-      // get mesh for this node
-      const mesh = modelJson.meshes[node.mesh];
-
-      // for each mesh primitive (almost there!!)
-      mesh.primitives.forEach(async (meshPrimitive) => {
-
-        const indexId = meshPrimitive.indices;
-        const attributesId = meshPrimitive.attributes.POSITION;
-
-        const indexAccessor = modelJson.accessors[indexId];
-        const vertexAccessor = modelJson.accessors[attributesId];
-
-        const indexBufferView = modelJson.bufferViews[indexAccessor.bufferView];
-        const vertexBufferView = modelJson.bufferViews[vertexAccessor.bufferView];
-
-        // let's get the indices loaded
-        const indexBuffer = await fetch(modelJson.buffers[indexBufferView.buffer].uri);
-        const indexBlob = await indexBuffer.blob();
-        // get the appropriate slice of the blob
-        const indexBlobSlice = indexBlob.slice(indexBufferView.byteOffset, indexBufferView.byteOffset + indexBufferView.byteLength);
-
-        const indices = new Int16Array(await indexBlobSlice.arrayBuffer());
-        // we have successfully created the index buffer!!! WOO!!
-        console.log(indices);
-
-        // if the vertex buffer is the same as the index buffer, reuse it. Otherwise, don't lol
-        let vertexBlob;
-        if (indexBufferView.buffer === vertexBufferView.buffer) vertexBlob = indexBlob;
-        else {
-          const vertexBuffer = await fetch(modelJson.buffers[vertexBufferView.buffer].uri);
-          vertexBlob = await vertexBuffer.blob();
-        }
-
-        const vertexBlobSlice = vertexBlob.slice(vertexBufferView.byteOffset, vertexBufferView.byteOffset + vertexBufferView.byteLength);
-        const vertices = new Float32Array(await vertexBlobSlice.arrayBuffer());
-        // this should be the vertex buffer! nice!
-
-        resolve({ buffers: { vertex: vertices }})
-
-      });
-
-    });*/
-
   });
 
 }

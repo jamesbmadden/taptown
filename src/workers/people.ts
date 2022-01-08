10f240f316f6374f9eca04e53ec68ff68ad0725c
James Madden <jamesbmadden@gmail.com>
2022-01-08 13:07:37 -0800

    add money and population to people worker

--- src/workers/people.ts
+++ src/workers/people.ts
@@ -2,8 +2,22 @@ import * as Comlink from "comlink";
 
 class People {
 
+  properties = {
+    money: 100,
+    population: 10
+  }
+
+  _cb: Function;
+
   static log () {
     console.log('people worker running');
+  }
+
+  setCallback (cb) {
+
+    this._cb = cb;
+    this._cb(this.properties);
+
   }
 
 }

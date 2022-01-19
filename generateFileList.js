e042dbdc061c16850af5b88149971993c235c942
James Madden <jamesbmadden@gmail.com>
2022-01-18 18:45:38 -0800

    Add system for reading the production output

--- generateFileList.js
+++ generateFileList.js
@@ -3,18 +3,43 @@
  * It looks at the output from Vite and uses it to generate a list of files for sw.js to cache.
  */
 
-import fs from 'fs';
+const fs = require('fs');
 
 const fileList = [];
 
 /**
  * get recursive list of all files in directory
  */
-function readFolder (dir) {
+function readFolder (dir, url, pushThisLevel) {
+
+  // read each file in the directory
+  fs.readdirSync(dir).forEach(filePath => {
+
+    // check the file
+    let file = fs.statSync(`${dir}/${filePath}`);
+
+    // if it's a folder, run readFolder on it
+    if (file && file.isDirectory()) {
+
+      readFolder(`${dir}/${filePath}`, `${url}/${filePath}`, true);
+
+    } 
+    // otherwise, IF we are pushing files on this level, add it to the list
+    else if (pushThisLevel) fileList.push(`${url}/${filePath}`);
+
+
+  });
 
 }
 
 // read from the "dist" dir
-readFolder(__dirname + 'dist');
+readFolder(`${__dirname}/dist`, '', false)
 
-console.log(fileList);
+// put quotes around the file list
+const fileListFormatted = fileList.map(file => `'${file}'`);
+
+// NOW, write this to dist/filesToCache.js
+const filesToCacheContent = `// This is the production version of this file.\nvar filesToCache = [ ${fileListFormatted.join(',')} ];`;
+
+// finally, write it out
+fs.writeFileSync(`${__dirname}/dist/filesToCache.js`, filesToCacheContent);

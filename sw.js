34c7519d77a569df5ceac0d158e1e35b70356e2b
jamesbmadden <39145924+jamesbmadden@users.noreply.github.com>
2022-01-20 00:49:15 +0000

    Deploying to gh-pages from  @ 3f1d98b3d1ec28bf0b9fc402d5d00874b1386ed2 🚀

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // this is the production version of this file :)
 // appVer is specific to THIS BUILD only and is used to manage caching.
 // Files to cache is a list of the file names actually used in this version.
-var appVer = '2022.0.20.0.34.49';
+var appVer = '2022.0.20.0.49.14';
 var filesToCache = [ '/app/index.html','/assets/Cafe.89231c7b.gltf','/assets/Road--l.cec3388c.gltf','/assets/Road-T.dd27be2f.gltf','/assets/Road-_l_.04dac8e2.gltf','/assets/Road-cross.c0149426.gltf','/assets/Road-down.3aa34828.gltf','/assets/Road-downright.2ee19715.gltf','/assets/Road-l-.8fa437e2.gltf','/assets/Road-left.e4d57c46.gltf','/assets/Road-leftdown.43e4587d.gltf','/assets/Road-right.05535041.gltf','/assets/Road-rightup.f9b3a2d6.gltf','/assets/Road-single.f2299542.gltf','/assets/Road-up.f33fbfbe.gltf','/assets/Road-upleft.8e8e8992.gltf','/assets/Road-x.25087994.gltf','/assets/Road-y.43b84893.gltf','/assets/ambient.6d469442.js','/assets/app.094664c2.css','/assets/app.79957490.js','/assets/buildings.a58a3922.js','/assets/db.ba7b3be2.js','/assets/landing.11d65904.css','/assets/landing.b6f501f9.js','/assets/outofbounds.54610d6d.gltf','/assets/people.9ab39b39.js','/assets/vendor.4f190134.js','/img/bg.mp4','/img/icons/logo-128.png','/img/icons/logo-192.png','/img/icons/logo-256.png','/img/icons/logo-32.png','/img/icons/logo-512.png','/img/icons/logo-64.png','/img/icons/logo-square-128.png','/img/icons/logo-square-192.png','/img/icons/logo-square-256.png','/img/icons/logo-square-32.png','/img/icons/logo-square-512.png','/img/icons/logo-square-64.png','/img/logo.png','/','/app/','/index.html' ];
 
 // the name to use for the cache
@@ -19,22 +19,10 @@ self.addEventListener('activate', async (event) => {
     if (cacheName !== cacheId) return caches.delete(cacheName);
   }));
 
-  /*// the part to trim from the start of filepaths
-  // the slash at the end should be kept
-  const urlBase = location.toString().slice(0, location.toString().length - 6);
-
-  // open the cache
-  const cache = await caches.open('taptown');
-  (await cache.keys()).forEach(file => {
-
-    // just get the section of the filename we want
-    const fileName = file.url.slice(urlBase.length, file.url.length);
-
-    // if the list of files to keep doesn't include this file, DELETE IT
-    if (!filesToCache.includes(fileName)) cache.delete(file);
-    
-
-  });*/
+  // open the cache for the current version
+  const cache = await caches.open(cacheId);
+  // add ALL the required files to cache, that way everything is downloaded and can be served from cache
+  cache.addAll(filesToCache);
 
 });
 

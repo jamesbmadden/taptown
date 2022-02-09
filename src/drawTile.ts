15396fb2e1cf8d9a2002e227c71a7b793e40f8e6
James Madden <jamesbmadden@gmail.com>
2022-02-08 18:23:33 -0800

    provide normals for shaders

--- src/drawTile.ts
+++ src/drawTile.ts
@@ -147,6 +147,24 @@ export default function drawTile (gl: WebGLRenderingContext, modelId: number, [x
         offset);
     gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
   }
+  // again with the normals
+  {
+    const numComponents = 3;  // pull out 2 values per iteration
+    const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+    const normalize = false;  // don't normalize
+    const stride = 0;         // how many bytes to get from one set of values to the next
+    const offset = 0;         // how many bytes inside the buffer to start from
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, model.buffers.normal);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.normalPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(programInfo.attribLocations.normalPosition);
+  }
   // Again with tex coords
   {
     const numComponents = 2;  // pull out 2 values per iteration

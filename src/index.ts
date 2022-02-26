30b8c05be78d5b4e9d16dddc618715dd2c3ab77c
James Madden <jamesbmadden@gmail.com>
2022-02-25 20:28:41 -0800

    Move saves to subgrid

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@ import '@pwabuilder/pwainstall';
 import loadDb, { getSavesList, GameSave } from './db';
 
 // get references to elements
-const homeCardsElement = document.querySelector('.home-cards');
+const saveListElement = document.querySelector('.save-list');
+const addButton = document.querySelector('.add-save');
+// @ts-ignore
+const newSaveName: HTMLInputElement = document.getElementById('new-save-name');
 
 // determine whether we are running in PWA mode?
 const isPwa = (new URLSearchParams(location.search)).get('pwa');
@@ -24,24 +27,11 @@ async function main () {
   // open a database. If one doesn't exist, create it.
   db = await loadDb();
 
-  addButtonListener();
-
-  // get list of saves to add to saves list
-  updateSavesList(await getSavesList(db));
-
-
-}
-
-function addButtonListener () {
-
-  const addButton = document.querySelector('.add-save');
-
   // when add button is clicked, add new save to indexedDB
   addButton.addEventListener('click', async () => {
 
     // get the new save name
-    // @ts-ignore
-    const key = document.getElementById('new-save-name').value;
+    const key = newSaveName.value;
 
     // pretend this is valid game data
     const emptySave: GameSave = {
@@ -67,6 +57,10 @@ function addButtonListener () {
 
   });
 
+  // get list of saves to add to saves list
+  updateSavesList(await getSavesList(db));
+
+
 }
 
 /**
@@ -74,13 +68,8 @@ function addButtonListener () {
  */
 function updateSavesList (savesList: Array<any>) {
 
-  // first clear out the saves
-  homeCardsElement.innerHTML = `<div class="card card-install" onclick="document.querySelector('pwa-install').openPrompt()">
-  <div class="card-inner">
-    <h2>Install TapTown</h2>
-    <p>Add TapTown to your homescreen for easy access and a full app experience.</p>
-  </div>
-</div>`;
+  // clear the save list
+  saveListElement.innerHTML = ``;
 
   savesList.forEach(save => {
 
@@ -90,18 +79,8 @@ function updateSavesList (savesList: Array<any>) {
     card.innerHTML = `<div class="card-inner"><h2>${save}</h2><p>Tap to Play!</p></div>`;
 
     // add to the savesListElement
-    homeCardsElement.appendChild(card);
+    saveListElement.appendChild(card);
 
   });
-
-  homeCardsElement.innerHTML += `<div class="card card-newsave">
-    <div class="card-inner">
-      <h2>Add New Save</h2>
-      <label for="new-save-name">New Save:</label><input id="new-save-name" name="new-save-name" type="string"><button class="add-save">Add</button>
-    </div>
-  </div>
-</div>`;
-
-  addButtonListener();
 
 }
